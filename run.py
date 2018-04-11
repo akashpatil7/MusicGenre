74dafe5bc97ca5ec95fc1e015ca2d86d3345c181
akashpatil7 <akash.aku97@gmail.com>
2018-04-11 21:56:58 +0530

    Update run.py

--- run.py
+++ run.py
@@ -13,3 +13,27 @@ GENRES = ['0', '1', '2', '3', '4', '5', '6','7','8','9','10']
 GENRE_LOOKUP = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal',
         'pop', 'reggae', 'rock']
 
+WINDOW_SIZE = 2048
+WINDOW_STRIDE = WINDOW_SIZE // 2
+N_MELS = 128
+MEL_KWARGS = {
+    'n_fft': WINDOW_SIZE,
+    'hop_length': WINDOW_STRIDE,
+    'n_mels': N_MELS
+}
+
+def load_track(filename, enforce_shape=None):
+    new_input, sample_rate = lbr.load(filename, mono=True)
+    features = lbr.feature.melspectrogram(new_input, **MEL_KWARGS).T
+    
+    if enforce_shape is not None:
+        if features.shape[0] < enforce_shape[0]:
+            delta_shape = (enforce_shape[0] - features.shape[0],
+                           enforce_shape[1])
+            features = np.append(features, np.zeros(delta_shape), axis=0)
+        elif features.shape[0] > enforce_shape[0]:
+            offset = features.shape[0] - enforce_shape[0]
+            features = features[offset :, :]
+
+    features[features == 0] = 1e-6
+    return (np.log(features), float(new_input.shape[0]) / sample_rate)
