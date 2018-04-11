22466bb4916b4a675648e53ae6e19db100f2df8b
akashpatil7 <akash.aku97@gmail.com>
2018-04-11 20:13:44 +0530

    Update run.py

--- run.py
+++ run.py
@@ -8,3 +8,8 @@ import argparse
 DEFAULT_SHAPE = (647,128)
 import librosa as lbr
 import keras.backend as K
+
+GENRES = ['0', '1', '2', '3', '4', '5', '6','7','8','9','10']
+GENRE_LOOKUP = ['blues', 'classical', 'country', 'disco', 'hiphop', 'jazz', 'metal',
+        'pop', 'reggae', 'rock']
+
