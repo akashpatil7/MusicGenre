888e5f434478729308e0069f4d6ebde6199de67b
akashpatil7 <akash.aku97@gmail.com>
2018-04-13 15:51:21 +0530

    Update and rename create_data_pickle.py to datapickle.py

--- datapickle.py
+++ datapickle.py
@@ -1,10 +1,40 @@
 from common import load_track, GENRES
+import matplotlib
+matplotlib.use('Agg')
+
 import sys
 import numpy as np
+import librosa as lbr
+import keras.backend as K
 from math import pi
 from pickle import dump
 import os
 from optparse import OptionParser
+
+GENRES = ['Hip-Hop','Pop','Folk/Country','Jazz','Rock/Metal','Reggae/Intl','Electronic','Instrumental']
+WINDOW_SIZE = 2048
+WINDOW_STRIDE = WINDOW_SIZE // 2
+N_MELS = 128
+MEL_KWARGS = {
+    'n_fft': WINDOW_SIZE,
+    'hop_length': WINDOW_STRIDE,
+    'n_mels': N_MELS
+}
+
+def load_track(filename, enforce_shape=None):
+    new_input, sample_rate = lbr.load(filename, mono=True)
+    features = lbr.feature.melspectrogram(new_input, **MEL_KWARGS).T
+
+    if enforce_shape is not None:
+        if features.shape[0] < enforce_shape[0]:
+            delta_shape = (enforce_shape[0] - features.shape[0],
+                    enforce_shape[1])
+            features = np.append(features, np.zeros(delta_shape), axis=0)
+        elif features.shape[0] > enforce_shape[0]:
+            features = features[: enforce_shape[0], :]
+
+    features[features == 0] = 1e-6
+    return (np.log(features), float(new_input.shape[0]) / sample_rate)
 
 TRACK_COUNT = 1000
 
